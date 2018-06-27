7e55858dfc5d0517f86f3a4c510e4071c28d1f17
epiqueras <epiquerass@gmail.com>
2018-06-26 21:25:06 -0500

    feat(dapp-api): add contract

--- src/bootstrap/dapp-api.js
+++ src/bootstrap/dapp-api.js
@@ -1,7 +1,11 @@
 import Web3 from 'web3'
 
+import ArbitrablePermissionList from '../assets/contracts/ArbitrablePermissionList.json'
+
 const env = process.env.NODE_ENV === 'production' ? 'PROD' : 'DEV'
 const ETHEREUM_PROVIDER = process.env[`REACT_APP_${env}_ETHEREUM_PROVIDER`]
+const ARBITRABLE_PERMISSION_LIST_ADDRESS =
+  process.env[`REACT_APP_${env}_ARBITRABLE_PERMISSION_LIST_ADDRESS`]
 
 let web3
 if (process.env.NODE_ENV === 'test')
@@ -34,10 +38,16 @@ const ETHAddressRegExpCaptureGroup = '(0x[a-fA-F0-9]{40})'
 const ETHAddressRegExp = /0x[a-fA-F0-9]{40}/
 const strictETHAddressRegExp = /^0x[a-fA-F0-9]{40}$/
 
+const arbitrablePermissionList = new web3.eth.Contract(
+  ArbitrablePermissionList.abi,
+  ARBITRABLE_PERMISSION_LIST_ADDRESS
+)
+
 export {
   web3,
   network,
   ETHAddressRegExpCaptureGroup,
   ETHAddressRegExp,
-  strictETHAddressRegExp
+  strictETHAddressRegExp,
+  arbitrablePermissionList
 }
