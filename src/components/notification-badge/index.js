a7e447edaba4c535b11e1031ace60a6c241c6d27
epiqueras <epiquerass@gmail.com>
2018-07-22 15:55:27 -0500

    fix(notification-badge): hide when there are no notifications

--- src/components/notification-badge/index.js
+++ src/components/notification-badge/index.js
@@ -53,11 +53,12 @@ export default class NotificationBadge extends PureComponent {
     const { isOpen } = this.state
     if (!notifications.data) return null
 
+    const hasNotifications = notifications.data.length > 0
     const useMaxShown = maxShown && notifications.data.length > maxShown
     return (
       <div className="NotificationBadge">
         {children}
-        {notifications.data.length > 0 && (
+        {hasNotifications && (
           <div
             onMouseEnter={this.handleMouseEnter}
             onMouseLeave={this.handleMouseLeave}
@@ -66,46 +67,47 @@ export default class NotificationBadge extends PureComponent {
             {notifications.data.length}
           </div>
         )}
-        {isOpen && (
-          <div
-            onMouseEnter={this.handleMouseEnter}
-            onMouseLeave={this.handleMouseLeave}
-            className="NotificationBadge-notifications"
-          >
-            {(useMaxShown
-              ? notifications.data.slice(0, maxShown)
-              : notifications.data
-            ).map(n => (
-              <div
-                key={n.ID}
-                id={n.ID}
-                onClick={onNotificationClick}
-                className="NotificationBadge-notifications-notification"
-              >
-                <img
-                  src={n.thumbnailURL}
-                  alt="Notification Thumbnail"
-                  className="NotificationBadge-notifications-notification-thumbnail"
-                />
-                <div className="NotificationBadge-notifications-notification-content">
-                  {n.message}
-                  <br />
-                  <small>
-                    <TimeAgo datetime={n.date} />
-                  </small>
+        {hasNotifications &&
+          isOpen && (
+            <div
+              onMouseEnter={this.handleMouseEnter}
+              onMouseLeave={this.handleMouseLeave}
+              className="NotificationBadge-notifications"
+            >
+              {(useMaxShown
+                ? notifications.data.slice(0, maxShown)
+                : notifications.data
+              ).map(n => (
+                <div
+                  key={n.ID}
+                  id={n.ID}
+                  onClick={onNotificationClick}
+                  className="NotificationBadge-notifications-notification"
+                >
+                  <img
+                    src={n.thumbnailURL}
+                    alt="Notification Thumbnail"
+                    className="NotificationBadge-notifications-notification-thumbnail"
+                  />
+                  <div className="NotificationBadge-notifications-notification-content">
+                    {n.message}
+                    <br />
+                    <small>
+                      <TimeAgo datetime={n.date} />
+                    </small>
+                  </div>
                 </div>
-              </div>
-            ))}
-            {useMaxShown && (
-              <div
-                onClick={onShowAll}
-                className="NotificationBadge-notifications-showAll"
-              >
-                <small>SHOW ALL</small>
-              </div>
-            )}
-          </div>
-        )}
+              ))}
+              {useMaxShown && (
+                <div
+                  onClick={onShowAll}
+                  className="NotificationBadge-notifications-showAll"
+                >
+                  <small>SHOW ALL</small>
+                </div>
+              )}
+            </div>
+          )}
       </div>
     )
   }
