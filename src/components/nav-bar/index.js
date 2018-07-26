c1cc24db1810067bf62c6517968e4e2ac91f47a4
epiqueras <epiquerass@gmail.com>
2018-07-26 11:20:54 -0500

    feat(navbar): make responsive

--- src/components/nav-bar/index.js
+++ src/components/nav-bar/index.js
@@ -1,60 +1,100 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { Link, NavLink } from 'react-router-dom'
+import { pushRotate as ReactBurgerMenu } from 'react-burger-menu'
+import debounce from 'debounce'
 
 import logo from '../../assets/images/logo.png'
 
 import './nav-bar.css'
 
-const NavBar = ({ routes, extras }) => (
-  <div className="NavBar">
-    <Link to="/">
-      <img src={logo} alt="Logo" className="NavBar-logo" />
-    </Link>
-    {routes.map(r => (
-      <div key={r.title} className="NavBar-route">
-        {r.isExternal ? (
-          <a
-            href={r.to}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="NavBar-route-link"
+export default class NavBar extends PureComponent {
+  static propTypes = {
+    // State
+    routes: PropTypes.arrayOf(
+      PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        to: PropTypes.string.isRequired,
+        isExternal: PropTypes.bool
+      }).isRequired
+    ).isRequired,
+    extras: PropTypes.arrayOf(PropTypes.node.isRequired).isRequired
+  }
+
+  state = {
+    isMobile: document.body.clientWidth < 780
+  }
+
+  constructor(props) {
+    super(props)
+    window.addEventListener('resize', this.handleWindowResize)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowResize)
+  }
+
+  handleWindowResize = debounce(({ currentTarget: { innerWidth } }) =>
+    this.setState({ isMobile: innerWidth < 780 })
+  )
+
+  render() {
+    const { routes, extras } = this.props
+    const { isMobile } = this.state
+
+    const logoImg = <img src={logo} alt="Logo" className="NavBar-logo" />
+    const routesAndExtras = [
+      ...routes.map(r => (
+        <div key={r.title} className="NavBar-route">
+          {r.isExternal ? (
+            <a
+              href={r.to}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="NavBar-route-link"
+            >
+              {r.title}
+            </a>
+          ) : (
+            <NavLink
+              exact
+              to={r.to}
+              className="NavBar-route-link"
+              activeClassName="is-active"
+            >
+              {r.title}
+            </NavLink>
+          )}
+        </div>
+      )),
+      ...extras.map((e, i) => (
+        <div
+          key={e.key}
+          className={`NavBar-extra ${i === 0 ? 'NavBar-extra--first' : ''}`}
+        >
+          {e}
+        </div>
+      ))
+    ]
+    return (
+      <div className="NavBar">
+        <Link to="/">{logoImg}</Link>
+        {isMobile ? (
+          <ReactBurgerMenu
+            pageWrapId="scroll-root"
+            outerContainerId="router-root"
+            customBurgerIcon={<div />}
+            customCrossIcon={logoImg}
+            className="NavBar-burgerMenu"
+            itemListClassName="NavBar-burgerMenu-itemList"
+            overlayClassName="NavBar-burgerMenu-overlay"
           >
-            {r.title}
-          </a>
+            {routesAndExtras}
+          </ReactBurgerMenu>
         ) : (
-          <NavLink
-            exact
-            to={r.to}
-            className="NavBar-route-link"
-            activeClassName="is-active"
-          >
-            {r.title}
-          </NavLink>
+          routesAndExtras
         )}
       </div>
-    ))}
-    {extras.map((e, i) => (
-      <div
-        key={e.key}
-        className={`NavBar-extra ${i === 0 ? 'NavBar-extra--first' : ''}`}
-      >
-        {e}
-      </div>
-    ))}
-  </div>
-)
-
-NavBar.propTypes = {
-  // State
-  routes: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      to: PropTypes.string.isRequired,
-      isExternal: PropTypes.bool
-    }).isRequired
-  ).isRequired,
-  extras: PropTypes.arrayOf(PropTypes.node.isRequired).isRequired
+    )
+  }
 }
-
-export default NavBar
